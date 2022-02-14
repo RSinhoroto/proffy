08176dbaa0c86fd62b6f1bfbb35ff8b14d786266
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-02-14 16:58:49 -0300

    Configured template engine (nunjucks)

--- src/server.js
+++ src/server.js
@@ -23,19 +23,35 @@ const proffys = [
   }
 ]
 
+
+
+function pageLanding (req, res) {
+  return res.render("index.html")
+}
+
+function pageStudy (req, res) {
+  return res.render("study.html", { proffys })
+}
+
+function pageGiveClasses (req, res) {
+  return res.render("give-classes.html")
+}
+
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
 
-.get("/", (req, res) => {
-  return res.sendFile(__dirname + "/views/index.html")
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
 })
-.get("/study", (req, res) => {
-  return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/give-classes", (req, res) => {
-  return res.sendFile(__dirname + "/views/give-classes.html")
-})
+
+server
+.use(express.static("public"))
+
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 
 .listen(5000)

907c6d9db2322773515a49dc11dfb19b88fe74a2
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-02-15 11:58:07 -0300

    Added template engine to study form

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const proffys = [
   },
   {
     name: "Diogo Fernandez", 
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+    avatar: "https://placekitten.com/185/185", 
     whatsapp: "89987654534", 
     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
     subject: "Química", 
@@ -23,6 +23,28 @@ const proffys = [
   }
 ]
 
+const subjects = [
+  "Artes",
+  "Biologia",
+  "Ciências",
+  "Educação física",
+  "Física",
+  "Geografia",
+  "História",
+  "Matemática",
+  "Português",
+  "Química",
+]
+
+const weekdays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+]
 
 
 function pageLanding (req, res) {
@@ -30,7 +52,8 @@ function pageLanding (req, res) {
 }
 
 function pageStudy (req, res) {
-  return res.render("study.html", { proffys })
+  const filters = req.query
+  return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses (req, res) {

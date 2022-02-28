7835a99587046804a779be79c4ec7b0b210d73cf
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-02-28 16:41:58 -0300

    Added database

---
+++ src/database/db.js
@@ -0,0 +1,21 @@
+const Database = require('sqlite-async')
+Database.open(__dirname + '/database.sqlite').then(execute)
+
+function execute(db) {
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS proffys (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      avatar TEXT,
+      whatsapp TEXT,
+      bio TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS classes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      subject TEXT,
+      cost TEXT,
+      prffy_id INTEGER
+    );
+  `)
+}

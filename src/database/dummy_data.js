0dcce7457c7056f272676bc86da5c1b60fa45b0c
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-03-14 11:27:25 -0300

    Added some server utilities

---
+++ src/database/dummy_data.js
@@ -0,0 +1,24 @@
+const proffys = [
+  { 
+    name: "Diego Fernandes", 
+    avatar: "https://placekitten.com/185/185", 
+    whatsapp: "89987654534", 
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química", 
+    cost: "20", 
+    weekday: [0], 
+    time_from: [720], 
+    time_to: [1220] 
+  },
+  {
+    name: "Diogo Fernandez", 
+    avatar: "https://placekitten.com/185/185", 
+    whatsapp: "99987654531", 
+    bio: "Entusiasta das melhores tecnologias de física avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Física", 
+    cost: "20", 
+    weekday: [1], 
+    time_from: [820], 
+    time_to: [1320] 
+  }
+]

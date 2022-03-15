8e7900adcc155df2fda039bd0daf3e63f0fdc939
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2022-03-15 16:23:10 -0300

    Added separate pages file

--- src/server.js
+++ src/server.js
@@ -1,30 +1,7 @@
-const { subjects, weekdays, getSubject } = require('./utils/format')
-
-function pageLanding (req, res) {
-  return res.render("index.html")
-}
-
-function pageStudy (req, res) {
-  const filters = req.query
-  return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses (req, res) {
-  const data = req.query
-
-  const isNotEmpty = Object.keys(data).length > 0
-  if (isNotEmpty) {
-    data.subject = getSubject(data.subject)
-    proffys.push(data)
-
-    return res.redirect("/study")
-  }
-  return res.render("give-classes.html", { weekdays, subjects })
-}
-
 const express = require('express')
 const server = express()
 
+const {pageLanding, pageStudy, pageGiveClasses} = require('./pages')
 
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
